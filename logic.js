6a7ccfc99e95f46350fba45b6e5416831fedb2f3
sja <skjameelakhtar2003@gmail.com>
2023-05-28 22:22:57 +0530

    Major UI improvements; Dark mode improvements

--- logic.js
+++ logic.js
@@ -1,28 +1,5 @@
 
 let userPics = document.getElementsByClassName('userpic');
-let usertestimoniallist = [`<i class="fa-sharp fa-solid fa-quote-left"></i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quaerat fugiat illum est deserunt odit doloribus sed ipsum reiciendis delectus.`,
-
-    `<i class="fa-sharp fa-solid fa-quote-left"></i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, facilis commodi? Quos enim esse minus!`,
-
-    `<i class="fa-sharp fa-solid fa-quote-left"></i>Lorem ipsum dolor sit amet.`,
-
-    `<i class="fa-sharp fa-solid fa-quote-left"></i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus tenetur expedita animi est? Dicta illum sed blanditiis ut?`,
-
-    `<i class="fa-sharp fa-solid fa-quote-left"></i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus tenetur expedita animi est? Dicta illum sed blanditiis ut?`,
-
-    `<i class="fa-sharp fa-solid fa-quote-left"></i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus tenetur expedita animi est? Dicta illum sed blanditiis ut?`,
-
-    `<i class="fa-sharp fa-solid fa-quote-left"></i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus tenetur expedita animi est? Dicta illum sed blanditiis ut?`,
-
-    `<i class="fa-sharp fa-solid fa-quote-left"></i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus tenetur expedita animi est? Dicta illum sed blanditiis ut?`,
-
-    `<i class="fa-sharp fa-solid fa-quote-left"></i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, facilis commodi? Quos enim esse minus!`,
-
-    `<i class="fa-sharp fa-solid fa-quote-left"></i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, facilis commodi? Quos enim esse minus!`,
-];
-
-// let thememode = document.getElementsByClassName('theme')[0];
-let bodytheme = document.getElementsByClassName('bodysection')[0];
 
 function changeTheme() {
     document.body.classList.toggle("darkmode");
@@ -34,8 +11,8 @@ function changeTheme() {
     }
 }
 
-x = document.getElementsByClassName('usertestimonial')[0];
-x.innerHTML = usertestimoniallist[0];
+// x = document.getElementsByClassName('usertestimonial')[0];
+// x.innerHTML = usertestimoniallist[0];
 function showReview() {
     for (userPic of userPics) {
         userPic.classList.remove('activeuser');
@@ -43,5 +20,5 @@ function showReview() {
 
     let i = Array.from(userPics).indexOf(event.target);
     userPics[i].classList.toggle('activeuser');
-    x.innerHTML = usertestimoniallist[i];
+    // x.innerHTML = usertestimoniallist[i];
 }
